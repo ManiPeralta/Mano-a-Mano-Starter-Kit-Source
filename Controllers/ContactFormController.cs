e692f41fefe7988e4fd0323c81562e65c9ecd557
Mani Peralta <mperalta@prominentmedia.com>
2021-06-27 23:48:59 +0100

    Added controller

---
+++ Controllers/ContactFormController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace B4.Controllers
+{
+    //public class ContactFormController : SurfaceController
+    //{
+    //    public IActionResult Index()
+    //    {
+    //        return View();
+    //    }
+    //}
+}
