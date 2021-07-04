6eec8f6b17c6cf6c9bdab3dbc230d207b1793484
Mani Peralta <mperalta@prominentmedia.com>
2021-07-04 13:08:08 +0100

    Worked on contact form

--- Controllers/ContactFormController.cs
+++ Controllers/ContactFormController.cs
@@ -4,14 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Umbraco.Cms.Web.Website.Controllers;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+using B4.Models;
 
 namespace B4.Controllers
 {
-    //public class ContactFormController : SurfaceController
-    //{
-    //    public IActionResult Index()
-    //    {
-    //        return View();
-    //    }
-    //}
+    public class ContactFormController : SurfaceController
+    {
+        string PATH = "~/Views/Partials/ContactForm/ContactForm.cshtml";
+        public ContactFormController(
+               IUmbracoContextAccessor umbracoContextAccessor,
+               IUmbracoDatabaseFactory databaseFactory,
+               ServiceContext services,
+               AppCaches appCaches,
+               IProfilingLogger profilingLogger,
+               IPublishedUrlProvider publishedUrlProvider)
+               : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+        public ActionResult RenderContactForm()
+        {
+            ContactModel model = new ContactModel();
+            return PartialView(PATH, model);
+        }
+    }
 }
---
+++ Models/ContactModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B4.Models
+{
+    public class ContactModel
+    {
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(80, ErrorMessage = "Name must be less than 80 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(100, ErrorMessage = "Name must be less than 100 characters")]
+        [EmailAddress(ErrorMessage = "Provide a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(150, ErrorMessage = "Subject must be less than 150 characters")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [MaxLength(500, ErrorMessage = "Message must be less than 500 characters")]
+        public string Message { get; set; }
+
+        public string RecaptchaSiteKey { get; set; }
+    }
+}
